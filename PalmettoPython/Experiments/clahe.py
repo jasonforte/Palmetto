007871815300565c9eee4ed54fc94defada2884f
Jason Forte <forte.business@gmail.com>
2014-09-23 18:41:18 +0200

    Created module to perform CLAHE

---
+++ PalmettoPython/Experiments/clahe.py
@@ -0,0 +1,22 @@
+'''
+Created on 23 Sep 2014
+A module to compute the contrast limited adaptive histogram enhancement method
+@author: jason
+'''
+
+import cv2
+import numpy
+import matplotlib.pyplot as plt
+
+
+
+def main():
+    pass
+
+
+
+
+
+""" Call the main method if initiated individually """
+if __name__ == '__main__':
+    main()

a59b22ca6575b8ece50f195cd5f91d585fcdea1f
Jason Forte <forte.business@gmail.com>
2014-09-30 22:34:41 +0200

    Created a histogram function which can build a matplotlib histogram of agiven image

---
+++ PalmettoPython/functions/__init__.py
@@ -0,0 +1,34 @@
+
+import numpy
+import matplotlib.pyplot
+
+def createHistogram(img, bins, figure=None):
+    """
+    createHistogram(ndarray img, int bins, matplotlib.figure.Figure figure) --> hist, bins
+    
+    Inputs:
+    img         -    image to find histogram
+    bins        -    number of histogram bins
+    figure      -    where to place the figure
+    
+    Returns:
+    hist        -    (tuple)  histogram element
+    bins        -    (int)    number of bins used
+    
+    Requires:    numpy, matplotlib.pyplot
+    
+    Description:
+    Displays a histogram of the image gray-scale values
+    
+    """
+    if figure == None:
+        matplotlib.pyplot.figure()
+    
+    """ Create a histogram of the flattened image """
+    hist,bins = numpy.histogram(img.flatten(), 256,[0,256])
+    matplotlib.pyplot.hist(img.flatten(), 256, [0,256], color = 'r')
+    matplotlib.pyplot.xlim([0,256])
+    
+    matplotlib.pyplot.title('Histogram of Image')
+    matplotlib.pyplot.show()
+    return hist,bins

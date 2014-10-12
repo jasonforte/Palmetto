3f3ad67c5085f12e9e2ff2762204f4978cad024a
DustioDesign <design@dustio.com>
2014-10-12 20:05:58 +0200

    Added structure.Base to contain system-wide configuration variables.

---
+++ PalmettoPython/structure/Base.py
@@ -0,0 +1,16 @@
+'''
+structure.Base.py
+
+Project: Palmetto
+
+Description: Contains system-wide configuration variables that setup basic paths for the project
+
+Created on Oct 12, 2014
+
+@author: Jason Forte
+@contact: <dev@dustio.com>
+'''
+
+
+base_dir = '/home/jason/Projects/Palmetto/PalmettoPython/'
+sample_dir = base_dir + 'tests/samples/'

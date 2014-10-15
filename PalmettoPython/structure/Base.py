e4b979a82b93b45090de57876ef1bfe52bb6f710
Jason Forte <forte.business@gmail.com>
2014-10-15 23:34:28 +0200

    Updated structure.Base Comments

---
+++ PalmettoPython/structure/Base.py
@@ -0,0 +1,24 @@
+'''
+structure.Base.py
+
+Project: Palmetto
+
+Description: Contains system-wide configuration variables that setup basic paths for the project
+
+Created on Oct 12, 2014
+
+@author:     Jason Forte
+
+@copyright:  2014 University of Cape Town. All rights reserved.
+
+@license:    GPL
+
+@contact:    <dev@dustio.com>
+@deffield    updated: Updated
+'''
+
+# The base directory represnts the project ROOT
+base_dir = '/home/jason/Projects/Palmetto/PalmettoPython/'
+
+# When an individual operation is run it loads sample images from this location
+sample_dir = base_dir + 'tests/samples/'

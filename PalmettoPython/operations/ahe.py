5c390180b834c1c1b95e9257317893fe6d908df6
Jason Forte <forte.business@gmail.com>
2014-10-16 09:21:46 +0200

    Final
    Signed-off-by: Jason Forte <forte.business@gmail.com>

--- PalmettoPython/operations/ahe.py
+++ PalmettoPython/operations/ahe.py
@@ -8,10 +8,11 @@ Created on 30 Sep 2014
 @author:  Jason Forte
 @contact: <dev@dustio.com>
 """
-
-import numpy
 import cv2
+import numpy
+
 import structure.Operation
+
 
 class AdaptiveHistogram(structure.Operation):
     '''   
@@ -38,4 +39,24 @@ class AdaptiveHistogram(structure.Operation):
         """
         output_image = cv2.equalizeHist(self.input_image)
         return output_image
+
+
+if __name__ == '__main__':
+    
+    import functions
+    import structure.Base
+    
+    image_file = structure.Base.sample_dir + 'sample.png'
+    
+    img = cv2.imread(image_file, 0)    
+    
+    op = AdaptiveHistogram(img)
+    
+    final = op.execute()
+    functions.createHistogram(final)
+    
+    cv2.imshow('Initial Image', img)
+    cv2.imshow('Result Image', final)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
    

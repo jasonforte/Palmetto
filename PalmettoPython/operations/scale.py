bf19f63bba34c9a7a4ee4afdf276c76e3eeb0ade
Jason Forte <forte.business@gmail.com>
2014-10-16 09:53:03 +0200

    Signed-off-by: Jason Forte <forte.business@gmail.com>

--- PalmettoPython/operations/scale.py
+++ PalmettoPython/operations/scale.py
@@ -18,6 +18,8 @@ class Downscale(Operation):
     Description
     Uses the opencv pyramid function to downsample the image
     '''
+    NAME = 'Down Scale'
+    
     def __init__(self, input_image, options=None):      
         # call to superclass
         super(Downscale, self).__init__(input_image, options)
@@ -32,29 +34,28 @@ class Upscale(Operation):
     Description
     Uses the opencv pyramid function to upsample the image
     '''
+    
+    NAME = 'Up Scale'
+    
     def __init__(self, input_image, options=None):      
         # call to superclass
         super(Upscale, self).__init__(input_image, options)
-        
+    
     def execute(self):
         return cv2.pyrUp(self.input_image)
 
 
 if __name__ == '__main__':
     
-    import os
+    import structure.Base
     
-    # setup directory
-    base = os.path.dirname(__file__)
-    dirname = os.path.join(base, '../tests/samples/clahe/')
+    sample_image = structure.Base.sample_dir + 'sample.png'
     
-    img1 = cv2.imread(dirname + "clahe3_res.png", 0)
+    img1 = cv2.imread(sample_image, 0)
     
-    op = Downscale(img1)   
-    img1 = op.execute()
-    op = Upscale(img1)
+    op = Upscale(img1)   
     final = op.execute()
     
-    cv2.imshow('Resulting Image', final)
+    cv2.imshow('result.png', final)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
